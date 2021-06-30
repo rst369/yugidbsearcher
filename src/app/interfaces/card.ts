876b4ad1b89d8c2ef7a2991031f5dd9a8d1fdd16
rst369 <rst_369@hotmail.com>
2021-06-30 12:48:40 -0300

    adicionando sets a div de informações das cartas

--- src/app/interfaces/card.ts
+++ src/app/interfaces/card.ts
@@ -1,3 +1,4 @@
+//import { Set } from './set';
 export interface Card {
     name: String;
     type: String;
@@ -8,4 +9,13 @@ export interface Card {
     race: String;
     attribute: String;
     id: Number;
+    card_sets: Set[];
+}
+
+export interface Set {
+    set_code: String;
+    set_name: String;
+    set_price: String;
+    set_rarity: String;
+    set_rarity_code: String;
 }
